7dd967cf263a6c3b30f539cf15b0e3311f415af6
Abraham Olude <abraham.olude.ao@gmail.com>
2023-10-31 05:23:31 +0000

    Complete Coding Challenge: Char Comparisons

--- src/main/java/CharComparison.java
+++ src/main/java/CharComparison.java
@@ -11,6 +11,24 @@ public class CharComparison {
      * @return -1 if A is less than B, 1 if A is greater than B, and 0 if the two arrays are identical.
      */
     public int compare(char[] a, char[] b){
+
+        int length = Math.min(a.length, b.length);
+        
+        for (int i = 0; i < length; i++) {
+            if (a[i] < b[i]) {
+                return -1;
+            }
+            else if (a[i] > b[i]) {
+                return 1;
+            }
+        }
+        
+        if (length < b.length) {
+            return -1;
+        }
+        else if (a.length > length) {
+            return 1;
+        }
         return 0;
     }
 }
